f54b397f8c5113375366a2476d8b18d24319ae05
Vinícius Gonçalves <viniciusebg25@icloud.com>
2024-07-25 18:30:43 -0300

    First commit

--- src/main/java/com/vebg/receitae/repository/ReceitaRepository.java
+++ src/main/java/com/vebg/receitae/repository/ReceitaRepository.java
@@ -1,2 +1,13 @@
-package com.vebg.receitae.repository;public interface ReceitaRepository {
+package com.vebg.receitae.repository;
+
+import com.vebg.receitae.entity.Ingrediente;
+import com.vebg.receitae.entity.Receita;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ReceitaRepository extends JpaRepository<Receita, UUID> {
+    public List<Receita> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 }
